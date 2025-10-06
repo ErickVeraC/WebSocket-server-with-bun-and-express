182d1d89b9eff7e939a41c36bb051ee5c7109313
Erick Vera Cuevas <erickverac90@gmail.com>
2025-10-06 11:29:11 -0600

    feat: Add type login for graphql

--- src/graphqlServer.ts
+++ src/graphqlServer.ts
@@ -1,21 +1,56 @@
 import { createYoga, createSchema } from "graphql-yoga";
 import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+//Models
+import User from "./models/User";
 
 const app = express();
 
 const yoga = createYoga({
   schema: createSchema({
     typeDefs: `
-        type Query {
-            hello: String!
+        type User {
+          id: ID!
+          name: String!
+          email: String!
         }
+        type Query {
+          users: [User!]!
+          hello: String!
+        }
+
+        type Mutation {
+          register(name: String!, email: String!, password: String!): User!
+          login(email: String!, password: String!): String! # Retorna un token
+        }
+
         type Subscription {
-            time: String!
+          time: String!
         }
         `,
     resolvers: {
       Query: {
         hello: () => "Hello from GraphQL in a Bun WS",
+        users: async () => {
+          return await User.find({});
+        },
+      },
+      Mutation: {
+        register: async (_: any, { name, email, password }: any) => {
+          const hashedPassword = await bcrypt.hash(password, 10);
+          const newUser = new User({ name, email, password: hashedPassword });
+          await newUser.save();
+          return newUser;
+        },
+        login: async (_: any, { email, password }: any) => {
+          const user = await User.findOne({ email });
+          if (!user) throw new Error("User not founded");
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch) throw new Error("Incorrect password");
+          const token = jwt.sign({ id: user._id }, "your_secret");
+        },
       },
       Subscription: {
         time: {
@@ -34,6 +69,6 @@ const yoga = createYoga({
 
 app.use("/graphql", yoga);
 
-app.listen(4000, () => {
+app.listen(3000, () => {
   console.log("Express server + Yoga en http://localhost:4000/graphql");
 });
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,20 @@
-import { Schema, model, models } from "mongoose";
+import { Schema, model, Document } from "mongoose";
 
-const userSchema = new Schema(
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema = new Schema<IUser>(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: String,
+    email: String,
+    password: String,
   },
   { timestamps: true }
 );
 
-const User = models.User || model("User", userSchema);
+const User = model<IUser>("User", UserSchema);
+
 export default User;
